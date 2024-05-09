bd40e27f3738ae06b0eef3160163c49c76bc3e39
Miguel Arpa Perozo <miguel.arpa.p@gmail.com>
2024-05-09 18:08:28 +0200

    whitespace cleanup

--- z-cl.c
+++ z-cl.c
@@ -18,7 +18,7 @@ usage()
 double
 compute_error(double ref, double output)
 {
-     return ref - output; 
+     return ref - output;
 }
 
 int
@@ -26,72 +26,72 @@ main(int argc, char *argv[])
 {
 
      /* system and controller transfer functions */
-     struct ztf sf; 
-     struct ztf cf; 
-     size_t linesize; 
-     char *line = NULL; 
+     struct ztf sf;
+     struct ztf cf;
+     size_t linesize;
+     char *line = NULL;
      char *time_str = NULL, *reference_str = NULL;
-     double s_init_v = 0.0, c_init_v = 0.0; 	/* intial output values */
+     double s_init_v = 0.0, c_init_v = 0.0;	/* intial output values */
 
      int opt;
-     char* sfname = NULL, *cfname = NULL; 
-     FILE *sfile = NULL, *cfile = NULL; 
+     char* sfname = NULL, *cfname = NULL;
+     FILE *sfile = NULL, *cfile = NULL;
 
      while ((opt = getopt(argc, argv, "c:s:i:j:")) != -1) {
 	  switch (opt) {
 	  case 'c':
 	       /* TODO: free fname */
-	       cfname = strdup(optarg); 
+	       cfname = strdup(optarg);
 	       /* printf("File name passed by argument: %s\n", cfname);  */
 	       break;
 	  case 's':
 	       /* TODO: free sfname */
-	       sfname = strdup(optarg); 
+	       sfname = strdup(optarg);
 	       /* printf("File name passed by argument: %s\n", sfname);  */
 	       break;
 	  case 'i':
-	       s_init_v = strtod(optarg, NULL); 
+	       s_init_v = strtod(optarg, NULL);
 	       /* printf("System initial value argument passed\n");  */
 	       break;
 	  case 'j':
-	       c_init_v = strtod(optarg, NULL); 
+	       c_init_v = strtod(optarg, NULL);
 	       /* printf("controller initial value argument passed\n");  */
 	       break;
 	  default: /* '?' */
 	       usage();
-	       fprintf(stderr, "Usage error\n"); 
+	       fprintf(stderr, "Usage error\n");
 	       exit(EXIT_FAILURE);
 	  }
-     }    
+     }
      /* Import system and controller transfer functions */
      if ( strlen(sfname) != 0 ) {
-	  sfile = fopen(sfname, "r"); 
+	  sfile = fopen(sfname, "r");
      }
 
      if ( sfile == NULL ) {
-	  fprintf(stderr, "Unable to open system transfer function file\n"); 
+	  fprintf(stderr, "Unable to open system transfer function file\n");
      }
      ztf_import(&sf, sfile);
      fclose(sfile);
-     free(sfname); 
+     free(sfname);
 
      if ( strlen(cfname) != 0 ) {
-	  cfile = fopen(cfname, "r"); 
+	  cfile = fopen(cfname, "r");
      }
 
      if ( cfile == NULL ) {
-	  fprintf(stderr, "Unable to open controller transfer function file\n"); 
+	  fprintf(stderr, "Unable to open controller transfer function file\n");
      }
      ztf_import(&cf, cfile);
      fclose(cfile);
-     free(cfname); 
+     free(cfname);
 
      /* Initialize transfer function with null values for the inputs
       * and outputs */
-     ztf_set_buf_input_vals(&sf, 0.0); 
+     ztf_set_buf_input_vals(&sf, 0.0);
      ztf_set_buf_output_vals ( &sf, s_init_v );
 
-     ztf_set_buf_input_vals(&cf, 0.0); 
+     ztf_set_buf_input_vals(&cf, 0.0);
      ztf_set_buf_output_vals ( &cf, c_init_v );
 
      /* printf("Controller transfer function: \n"); */
@@ -104,11 +104,11 @@ main(int argc, char *argv[])
 
 
      double time = 0.0, reference = 0.0, error = 0, control = 0;
-     double output = s_init_v; 	/* intial system output */
+     double output = s_init_v;	/* intial system output */
 
      while ( getline(&line, &linesize, stdin) > 0 ) {
-	  time_str = get_col(line, COL_TIME); 
-	  reference_str = get_col(line, COL_REF); 
+	  time_str = get_col(line, COL_TIME);
+	  reference_str = get_col(line, COL_REF);
 
 	  if ( time_str  == NULL || reference_str == NULL ) {
 	       fprintf(stderr, "Unable to allocate memory for column selection");
