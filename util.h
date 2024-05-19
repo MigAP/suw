5f96a121dcb2d48fa6987f48fe1fecfeb0b74778
Miguel Arpa Perozo <miguel.arpa.p@gmail.com>
2024-05-19 19:31:51 +0200

    reduce float equality comparison

--- util.h
+++ util.h
@@ -7,7 +7,7 @@
 #include <errno.h>
 
 #define PRECISION (-17)		   /* printf precision */
-#define EQUALITY_THRESHOLD (1e-12) /* value used to test floats equality */
+#define EQUALITY_THRESHOLD (1e-6) /* value used to test floats equality */
 #define TOKEN_DELIM " \t"	   /* whitespace or tab */
 #define ERROR (-1) 
 #define SUCCESS (0) 
