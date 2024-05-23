a151c5f2048e076f13d42df2ee5b4118ddd28a0d
Miguel Arpa Perozo <miguel.arpa.p@gmail.com>
2024-05-23 11:06:43 +0200

    add block test z-tf

--- tests/test_blocks.sh
+++ tests/test_blocks.sh
@@ -17,3 +17,8 @@ strtest step "0	0
 1	0
 2	2
 3	2" "$(../time -i 0 -f 3 -s 1 | ../step -i 0 -f 2 -t 2)"
+
+
+strtest z-tf "1	4	11
+2	5	16
+3	6	22" "$(../z-tf -f z_trivial_tf.txt  -i 7.0 < trivial_test.dat)"
